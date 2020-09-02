d24089ed2f26a2122d158c5a7e0a628137f2c094
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-02 04:55:06 +0100

    hmm

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ module.exports = async (req, res) => {
     } = req.query;
   
     res.setHeader("Content-Type", "image/png");
+    console.log("issues", issues)
   
     // if (blacklist.includes(username)) {
     //   return res.send(renderError("Something went wrong"));
@@ -26,9 +27,18 @@ module.exports = async (req, res) => {
   
       const comments = [
         'https://github.com/open-wa/wa-automate-nodejs/issues/747#issuecomment-682116171',
-        ...issues
+        "https://github.com/open-wa/wa-automate-nodejs/issues/325#issuecomment-614747409",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/297#issuecomment-612183849",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/304#issuecomment-612975003",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/98#issuecomment-583237115",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/407#issuecomment-625052304",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/277#issuecomment-609538724",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/261#issuecomment-609403404",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/260#issuecomment-609550067",
+        "https://github.com/open-wa/wa-automate-nodejs/issues/82#issuecomment-586136992"
     ]
     var comment = comments[Math.floor(Math.random() * comments.length)];
+    console.log("comment", comment)
       // res.setHeader("Cache-Control", `public, max-age=${cacheSeconds}`);
 
     const x = await getScreenshot(comment);
