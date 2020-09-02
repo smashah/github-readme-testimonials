a70c5fd4ac3fc728c7ac33ed171cdfdb8baba823
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-02 05:37:42 +0100

    parse array from base64

--- api/index.js
+++ api/index.js
@@ -4,9 +4,10 @@ module.exports = async (req, res) => {
   const {
     issues
   } = req.query;
-
   // res.setHeader("Content-Type", "image/png");
-  console.log("issues", req.query, issues)
+  console.log("issues", req.query, issues, typeof issues)
+  issues = JSON.parse(Buffer.from(issues, 'base64').toString('ascii').replace(/'/g,'"'));
+  if(!issues.length) return false;
   try {
     // const cacheSeconds = clampValue(
     //   parseInt(cache_seconds || CONSTANTS.TWO_HOURS, 10),
@@ -15,16 +16,7 @@ module.exports = async (req, res) => {
     // );
 
     const comments = [
-      'https://github.com/open-wa/wa-automate-nodejs/issues/747#issuecomment-682116171',
-      "https://github.com/open-wa/wa-automate-nodejs/issues/325#issuecomment-614747409",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/297#issuecomment-612183849",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/304#issuecomment-612975003",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/98#issuecomment-583237115",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/407#issuecomment-625052304",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/277#issuecomment-609538724",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/261#issuecomment-609403404",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/260#issuecomment-609550067",
-      "https://github.com/open-wa/wa-automate-nodejs/issues/82#issuecomment-586136992"
+      ...issues
     ]
     var comment = comments[Math.floor(Math.random() * comments.length)];
     console.log("comment", comment)
