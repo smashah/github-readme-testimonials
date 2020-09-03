0db677b677f0041bcaf6079daae5da12c8a94f30
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-03 05:54:32 +0100

    added cache_seconds as param

--- api/index.js
+++ api/index.js
@@ -17,13 +17,15 @@ module.exports = async (req, res) => {
     issues,
     svg,
     camo,
-    noCache = false
+    noCache = false,
+    cache_seconds
   } = req.query;
   console.log(
     issues,
     svg,
     camo,
-    noCache)
+    noCache,
+    cache_seconds)
   if (!noCache) {
     const cacheSeconds = clampValue(
       parseInt(cache_seconds || CONSTANTS.TWO_HOURS, 10),
