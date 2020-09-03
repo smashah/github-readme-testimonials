cbc0195b8713a427d5f1875ca06d70e93f494bb5
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-03 03:18:33 +0100

    added camo support

--- api/index.js
+++ api/index.js
@@ -3,13 +3,14 @@ const { getScreenshot } = require('./ppt');
 module.exports = async (req, res) => {
   let {
     issues,
-    svg
+    svg,
+    camo
   } = req.query;
   // res.setHeader("Content-Type", "image/png");
   try {
-    if(svg) {
+    if(svg || camo) {
       res.setHeader("Content-Type", "image/svg+xml");
-      return res.send(`<svg xmlns="http://www.w3.org/2000/svg"><style>.loader-path{stroke-dasharray:150,200;stroke-dashoffset:-10;-webkit-animation:dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;animation:dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;stroke-linecap:round}@-webkit-keyframes rotate{100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes rotate{100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@-webkit-keyframes dash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}@keyframes dash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}@-webkit-keyframes color{0%{stroke:#70c542}40%{stroke:#70c542}66%{stroke:#70c542}80%,90%{stroke:#70c542}}@keyframes color{0%{stroke:#70c542}40%{stroke:#70c542}66%{stroke:#70c542}80%,90%{stroke:#70c542}}</style> <circle class="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2"/> <image id="myimage" href="https://github-readme-testimonials.vercel.app/api?issues=${escape(`${issues}`)}"/> </svg>`);
+      return res.send(`<svg xmlns="http://www.w3.org/2000/svg"><style>.loader-path{stroke-dasharray:150,200;stroke-dashoffset:-10;-webkit-animation:dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;animation:dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;stroke-linecap:round}@-webkit-keyframes rotate{100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes rotate{100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@-webkit-keyframes dash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}@keyframes dash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}@-webkit-keyframes color{0%{stroke:#70c542}40%{stroke:#70c542}66%{stroke:#70c542}80%,90%{stroke:#70c542}}@keyframes color{0%{stroke:#70c542}40%{stroke:#70c542}66%{stroke:#70c542}80%,90%{stroke:#70c542}}</style> <circle class="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2"/> <image id="myimage" href="${camo}"/> </svg>`);
     }
     // const cacheSeconds = clampValue(
     //   parseInt(cache_seconds || CONSTANTS.TWO_HOURS, 10),
